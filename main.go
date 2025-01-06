0fdefe14e022292d8e9067127182d719648d9bc4
lepc1972 <githublepc@gmail.com>
2025-01-06 13:41:03 -0500

    fixes

--- main.go
+++ main.go
@@ -3,26 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	//var "watch TWN golang full course") = "watch TWN golang full course"
-	var taskone = "watch our go crash course"
+	fullGolang := "watch TWN golang full course"
+	shortGolang := "watch our go crash course"
+	rewardCoffee := "Reward myself with a cup of coffee ☕"
+
+	taskItems := []string{fullGolang, shortGolang, rewardCoffee} // slice
+	//var taskItems = [20]string{fullGolang, shortGolang, rewardCoffee} array
 	fmt.Println("##### Welcome to our todolist app #####")
+
 	fmt.Println("List of my todos")
-	fmt.Println(taskone)
-	fmt.Println("watch TWN golang full course")
-	fmt.Println("watch TWN golang full course")
-	fmt.Println("Reward myself with a cup of coffee ☕")
 
-	fmt.Println(taskone)
-	fmt.Println("watch TWN golang full course")
-	fmt.Println(taskone)
-	fmt.Println("watch TWN golang full course")
+	for i, task := range taskItems {
+		fmt.Println(i+1, task)
 
-	fmt.Println()
-	fmt.Println("Rewards")
-	fmt.Println("Reward myself with a cup of coffee ☕")
-
-	fmt.Println()
-	fmt.Println("My project")
-	fmt.Println(taskone)
+	}
 
 }
