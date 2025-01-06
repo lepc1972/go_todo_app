b54d8c42f5f27ee7ac4b4ee0f6e2e32dfc0e0dac
lepc1972 <githublepc@gmail.com>
2025-01-06 08:37:16 -0500

    vars

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	//var "watch TWN golang full course") = "watch TWN golang full course"
+	var taskone = "watch our go crash course"
+	fmt.Println("##### Welcome to our todolist app #####")
+	fmt.Println("List of my todos")
+	fmt.Println(taskone)
+	fmt.Println("watch TWN golang full course")
+	fmt.Println("watch TWN golang full course")
+	fmt.Println("Reward myself with a cup of coffee ☕")
+
+	fmt.Println(taskone)
+	fmt.Println("watch TWN golang full course")
+	fmt.Println(taskone)
+	fmt.Println("watch TWN golang full course")
+
+	fmt.Println()
+	fmt.Println("Rewards")
+	fmt.Println("Reward myself with a cup of coffee ☕")
+
+	fmt.Println()
+	fmt.Println("My project")
+	fmt.Println(taskone)
+
+}
